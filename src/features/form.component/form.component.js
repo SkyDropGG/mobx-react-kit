0137be08229c7f9e123814bbbb3e58b3424aa3ea
Denis Kozak <intern.frontend@jelvix.com>
2017-03-20 13:36:46 +0200

    Add form

--- src/features/form.component/form.component.js
+++ src/features/form.component/form.component.js
@@ -3,9 +3,36 @@ import {inject, observer} from 'mobx-react'
 
 @inject('stores') @observer
 export default class FormComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      form: props.stores.form.getForm()
+    };
+  }
+
   render() {
-    console.log(this.props.stores);
-    return (<h1>Form</h1>);
+    return (
+      <form>
+        <label htmlFor={this.state.form.$('email').id}>
+          {this.state.form.$('email').label}
+        </label>
+        <input {...this.state.form.$('email').bind()} />
+        <p>{this.state.form.$('email').error}</p>
+
+        <label htmlFor={this.state.form.$('password').id}>
+          {this.state.form.$('password').label}
+        </label>
+        <input {...this.state.form.$('password').bind({type: 'password'})} />
+        <p>{this.state.form.$('password').error}</p>
+
+        <button type="submit" onClick={this.state.form.onSubmit}>Submit</button>
+        <button type="button" onClick={this.state.form.onReset}>Reset</button>
+        <button type="button" onClick={this.state.form.onClear}>Clear</button>
+
+        <p>{this.state.form.error}</p>
+      </form>
+
+    );
   }
 }
 
--- src/features/form.component/form.store.js
+++ src/features/form.component/form.store.js
@@ -1,8 +1,39 @@
 import {observable, action} from 'mobx'
+import MobxReactForm from 'mobx-react-form';
+import validatorjs from 'validatorjs';
+
+const plugins = {dvr: validatorjs};
+
+class LoginForm extends MobxReactForm {
+  onSuccess(form) {
+    console.log('Form Values!', form.values());
+  }
+
+  onError(form) {
+    form.invalidate('This is a generic error message!');
+  }
+
+  onChange(form) {
+    console.log(form)
+  }
+}
 
 export default class FormStore {
-  @observable actionsList = 'asdasdsad';
-  @action addAction(actionName) {
-    this.actionsList.push(actionName);
+  @observable fields = {
+    email: {
+      label: 'Email',
+      placeholder: 'Insert Email',
+      rules: 'required|email|string|between:5,25',
+      type: 'text'
+    },
+    password: {
+      label: 'Password',
+      placeholder: 'Insert Password',
+      rules: 'required|string|between:5,25',
+    },
+  };
+
+  getForm() {
+    return new LoginForm({fields: this.fields}, {plugins});
   }
 }
